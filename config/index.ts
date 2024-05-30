a19a06ef25ad2be0f00bd11fc4c8f2eb5e5c0b87
miloje nikolic <nikolic.miloje0507@gmail.com>
2024-05-30 21:38:48 +0200

    format

--- config/index.ts
+++
@@ -1,2 +0,0 @@
-export { PORT, JWT_SECRET } from "./config";
-export { connectMongoDB } from './db'
--- utils/types.ts
+++
@@ -1,7 +0,0 @@
-export interface IStakingResult {
-  user: string;
-  count: bigint;
-  stakedAmount: bigint;
-  time: bigint;
-  period: bigint;
-}
