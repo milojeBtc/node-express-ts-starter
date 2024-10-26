898642e9e4a56f3b093f81f78bba8e915ca28eb2
CryptoHumanz <nikolic.miloje0507@gmail.com>
2024-10-26 18:43:18 +0100

    Update index.ts - add whitelist ip address and stress terst

--- index.ts
+++ index.ts
@@ -7,6 +7,7 @@ import path from 'path';
 import { PORT, connectMongoDB } from "./config";
 import http from "http";
 import { UserRouter } from "./routes";
+import rateLimit from "express-rate-limit";
 
 // Load environment variables from .env file
 dotenv.config();
@@ -16,6 +17,21 @@ connectMongoDB();
 
 // Create an instance of the Express application
 const app = express();
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100, // Limit each IP to 100 requests per windowMs
+  // delayMs: 0 // Disable delaying - full speed until the max limit is reached
+});
+
+// Apply rate limiting to /api endpoints
+app.use('/api', limiter); 
+
+// Store blocked IP addresses
+const blockedIPs = ['192.168.23.13', '172.16.1.1', '51.75.188.0'];
+
+// Store Whitelist IP addresses
+const whitelistIPs = ['146.19.215.121'];
 
 // Set up Cross-Origin Resource Sharing (CORS) options
 app.use(cors());
@@ -28,6 +44,24 @@ app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 app.use(bodyParser.json({ limit: '50mb' }));
 app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
+
+// app.use((req, res, next) => {
+//   console.log("req.ip ==> ", req.ip?.replace("::ffff:", ""));
+//   // @ts-ignore
+//   if (blockedIPs.includes(req.ip?.replace("::ffff:", ""))) {
+//     return res.status(403).send('Forbidden');
+//   }
+//   next();
+// });
+
+app.use((req, res, next) => {
+  console.log("req.ip ==> ", req.ip?.replace("::ffff:", ""));
+  // @ts-ignore
+  if (!whitelistIPs.includes(req.ip?.replace("::ffff:", ""))) {
+    return res.status(403).send('This IP address is not allowed to request backend.');
+  }
+  next();
+});
 
 const server = http.createServer(app);
 
